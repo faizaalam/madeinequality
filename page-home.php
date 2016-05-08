7f86e42d159ee3c8421137d7ae680e1960eee6d4
faizaalam <faiza.alam07@gmail.com>
2016-05-08 14:39:18 +0600

    lots and lot of changes

---
+++ page-home.php
@@ -0,0 +1,186 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * @package madeinequality
+
+ */
+
+get_header();
+ ?>
+
+ <div class="container">
+   <div class="row">
+    <div class="large-8 medium-8 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+            <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+                <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                            <span class="post-category">
+                                        Hiking
+                            </span>
+                                <span class="post-category post-featured">Featured</span>
+                        </span>
+                            <span class="preload-background-spinner spinner" style="display: none;">
+                                <span class="double-bounce1"></span>
+                                <span class="double-bounce2"></span>
+                            </span>
+                        <header class="post-header">
+                            <h2 class="post-title">Discover Redwood National Park</h2>
+                            <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                        </header></div>
+
+            </a>
+        </article></div>
+    <div class="large-4 medium-4 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+            <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+                <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                            <span class="post-category">
+                                        Hiking
+                            </span>
+                                <span class="post-category post-featured">Featured</span>
+                        </span>
+                            <span class="preload-background-spinner spinner" style="display: none;">
+                                <span class="double-bounce1"></span>
+                                <span class="double-bounce2"></span>
+                            </span>
+                        <header class="post-header">
+                            <h2 class="post-title">
+logo
+Features
+Destinations
+Hiking
+Visit France
+CITY GUIDE
+Five Reasons to Visit the Eiffel Tower</h2>
+                            <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                        </header></div>
+
+            </a>
+        </article></div>
+  </div>
+  <div class="row">
+   <div class="large-4 medium-4 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+           <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+               <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                           <span class="post-category">
+                                       Hiking
+                           </span>
+                               <span class="post-category post-featured">Featured</span>
+                       </span>
+                           <span class="preload-background-spinner spinner" style="display: none;">
+                               <span class="double-bounce1"></span>
+                               <span class="double-bounce2"></span>
+                           </span>
+                       <header class="post-header">
+                           <h2 class="post-title">Great Smoky Mountains National park</h2>
+                           <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                       </header></div>
+
+           </a>
+       </article></div>
+   <div class="large-4 medium-4 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+           <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+               <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                           <span class="post-category">
+                                       Hiking
+                           </span>
+                               <span class="post-category post-featured">Featured</span>
+                       </span>
+                           <span class="preload-background-spinner spinner" style="display: none;">
+                               <span class="double-bounce1"></span>
+                               <span class="double-bounce2"></span>
+                           </span>
+                       <header class="post-header">
+                           <h2 class="post-title">Discover Redwood National Park</h2>
+                           <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                       </header></div>
+
+           </a>
+       </article></div>
+   <div class="large-4 medium-4 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+           <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+               <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                           <span class="post-category">
+                                       Hiking
+                           </span>
+                               <span class="post-category post-featured">Featured</span>
+                       </span>
+                           <span class="preload-background-spinner spinner" style="display: none;">
+                               <span class="double-bounce1"></span>
+                               <span class="double-bounce2"></span>
+                           </span>
+                       <header class="post-header">
+                           <h2 class="post-title">Great Smoky Mountains National park</h2>
+                           <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                       </header></div>
+
+           </a>
+       </article></div>
+ </div>
+ <div class="row">
+   <div class="large-3 medium-3 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+           <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+               <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                           <span class="post-category">
+                                       Hiking
+                           </span>
+                               <span class="post-category post-featured">Featured</span>
+                       </span>
+                           <span class="preload-background-spinner spinner" style="display: none;">
+                               <span class="double-bounce1"></span>
+                               <span class="double-bounce2"></span>
+                           </span>
+                       <header class="post-header">
+                           <h2 class="post-title">Great Smoky Mountains National park</h2>
+                           <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                       </header></div>
+
+           </a>
+       </article></div>
+   <div class="large-6 medium-6 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+           <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+               <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                           <span class="post-category">
+                                       Hiking
+                           </span>
+                               <span class="post-category post-featured">Featured</span>
+                       </span>
+                           <span class="preload-background-spinner spinner" style="display: none;">
+                               <span class="double-bounce1"></span>
+                               <span class="double-bounce2"></span>
+                           </span>
+                       <header class="post-header">
+                           <h2 class="post-title">Discover Redwood National Park</h2>
+                           <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                       </header></div>
+
+           </a>
+       </article></div>
+   <div class="large-3 medium-3 small-12 columns"><article class="post preload-background" data-image="/hit-the-road/content/images/2015/10/8634008094_11aef35713_k.jpg" data-resize="true">
+
+           <a href="/hit-the-road/discover-redwood-national-park/" class="post-image">
+               <div class="preload-background-image" style="opacity: 1;">        <span class="post-category-wrap">
+                           <span class="post-category">
+                                       Hiking
+                           </span>
+                               <span class="post-category post-featured">Featured</span>
+                       </span>
+                           <span class="preload-background-spinner spinner" style="display: none;">
+                               <span class="double-bounce1"></span>
+                               <span class="double-bounce2"></span>
+                           </span>
+                       <header class="post-header">
+                           <h2 class="post-title">Discover Redwood National Park</h2>
+                           <p class="post-meta"><span class="post-meta-author"><i class="fa fa-user"></i>Karen Smith</span> <time class="post-meta-date" datetime="2015-10-25">25 Oct 2015</time></p>
+                       </header></div>
+
+           </a>
+       </article></div>
+</div>
+ </div>
